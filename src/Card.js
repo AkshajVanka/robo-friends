40d8e77d05e2dd72041a161b85020e01857539c0
Sai Akshaj Vanka <akshajv2000@gmail.com>
2023-12-07 02:16:20 +0530

    removed an extra log

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,6 @@ const Card = (props) => {
     const {
         name, email, id, set
     } = props;
-    console.log(set);
     const src = `https://robohash.org/${id}?size=200x200&set=set${set}`;
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
